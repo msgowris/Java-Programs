11fce640451f71acf138a12256b18b9e9f9fb374
Gowri S <Gowri S@DESKTOP-AF33508.Dlink>
2023-05-03 08:43:02 +0530

    java program to reverse a string without using String inbuilt function

--- Java Programming/src/string_programs/Reverse_String.java
+++ Java Programming/src/string_programs/Reverse_String.java
@@ -15,8 +15,17 @@ public class Reverse_String {
 		for (int i = original_string.length() - 1; i >= 0; i--) {
 			reverse_string = reverse_string + original_string.charAt(i);
 		}
-		System.out.println("Reverse String: " + reverse_string);
+		System.out.println("Reverse string without using string inbuilt function: " + reverse_string);
 
+		// java program to reverse a string using String inbuilt function
+		StringBuffer string_buffer = new StringBuffer("Gowri");
+		string_buffer.reverse();
+		System.out.println("Reverse string using stringbuffer inbuilt function: " + string_buffer);
+
+		// java program to reverse a string using String inbuilt function
+		StringBuilder string_builder = new StringBuilder("Karthick");
+		string_builder.reverse();
+		System.out.println("Reverse string using stringbuilder inbuilt function: " + string_builder);
 	}
 
 }
